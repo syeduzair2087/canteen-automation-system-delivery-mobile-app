a2f29a66365d60c5bc52a5c6ac206aa3ecdda1c0
syeduzair2087 <syeduzair.2087@gmail.com>
2017-04-14 13:07:52 +0500

    Job count and nav bar color fix

--- src/services/order-service.ts
+++ src/services/order-service.ts
@@ -9,12 +9,12 @@ export class OrderService {
 
     getOrders() {
         this.angularFire.database.list('/orders/'
-        , {
-            query: {
-                orderByChild: 'status/staffMemberId',
-                equalTo: localStorage.getItem('uid')
+            , {
+                query: {
+                    orderByChild: 'status/staffMemberId',
+                    equalTo: localStorage.getItem('uid')
+                }
             }
-        }
         ).subscribe((data) => console.log(data));
 
         return this.angularFire.database.list('/orders/', {
@@ -33,10 +33,19 @@ export class OrderService {
         loading.present();
         return new Promise((res, rej) => {
             this.angularFire.database.object('/orders/' + key).subscribe((data: Order) => {
-                loading.dismiss().catch(() => {});
+                loading.dismiss().catch(() => { });
                 res(data);
             });
         });
+    }
+
+    getJobCount() {
+        return new Promise((res, rej) => {
+            let countSubscription = this.angularFire.database.object('/roles/delivery_boys/' + localStorage.getItem('uid')).subscribe((data) => {
+                res(data);
+                countSubscription.unsubscribe();
+            })
+        })
     }
 
     updateOrderStatus(orderId: string, status: string) {
@@ -48,13 +57,23 @@ export class OrderService {
             this.angularFire.database.object('/orders/' + orderId + '/status').update({
                 state: status
             }).then(() => {
+                if (status == "Order Delivered") {
+                    this.getJobCount().then((count: number) => {
+                        this.angularFire.database.object('roles/delivery_boys/' + localStorage.getItem('uid')).update({
+                            job_count: <number>count - 1
+                        }).then(() => {
+                            res();
+                        }).catch(() => {
+                            rej();
+                        });
+                    })
+                }
                 this.toastCtrl.create({
                     message: 'Order updated.',
                     duration: 4500
                 }).present().then(() => loading.dismiss());
                 res();
-            }).catch(() => 
-            {
+            }).catch(() => {
                 loading.dismiss().then(() => rej());
             });
         })
